86b5efa70393dd4d09c0c277f745b92ca0219e5c
Syaiful Imanudin <112718167+SaipulImdn@users.noreply.github.com>
2023-03-14 20:02:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let saldoAwal = 90000
+let saldoTambahan = 50000
+const saldoAkhir = saldoAwal + saldoTambahan
+
+alert(`saldo awal kamu sebesar RP.${saldoAwal} 
+dan kamu menambah saldo kamu adalah RP.${saldoTambahan} 
+kemudian saldo yang kamu miliki adalah RP.${saldoAkhir}`)
